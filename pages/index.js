adfa2efa2c4265eca9b25567e0ca2802c8effc15
yyogesh <yogeshggn1@gmail.com>
2022-04-12 11:46:00 +0530

    get static path

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      Next JS pre-rendering
-    </div>
+    <>
+      <div className={styles.container}>
+        Next JS pre-rendering
+      </div>
+      <Link href='/users'>
+        <a>Users</a>
+      </Link>
+      <Link href='/posts'>
+        <a>Posts</a>
+      </Link>
+    </>
   )
 }

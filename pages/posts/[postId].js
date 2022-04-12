4a4762486f08ed0428e26a53ceb2aaa63216a5bd
yyogesh <yogeshggn1@gmail.com>
2022-04-12 13:47:06 +0530

    ISR revalidate

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 function Post({ post }) {
+    const router = useRouter();
+    if (router.isFallback) {
+        return <h1>Loading....</h1>
+    }
     return <>
         <h2>{post.id} {post.title}</h2>
         <p>{post.body}</p>
@@ -8,6 +14,15 @@ function Post({ post }) {
 export default Post
 
 export async function getStaticPaths() {
+    // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    // const data = await response.json();
+    // const paths = data.map(post => {
+    //     return {
+    //         params: {
+    //             postId: `${post.id}`
+    //         }
+    //     }
+    // })
     return {
         paths: [{
             params: { postId: '1' }
@@ -16,7 +31,8 @@ export async function getStaticPaths() {
         }, {
             params: { postId: '3' }
         }],
-        fallback: false
+        // paths,
+        fallback: true
     }
 }
 
@@ -24,7 +40,12 @@ export async function getStaticProps(context) {
     const { params } = context;
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
     const data = await response.json();
-
+    console.log(`Generating page for /post/${params.postId}`)
+    if (!data.id) {
+        return {
+            notFound: true
+        }
+    }
     return {
         props: {
             post: data
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -24,7 +24,6 @@ export default PostList;
 export async function getStaticProps() {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await response.json();
-
     return {
         props: {
             posts: data.slice(0, 3)
